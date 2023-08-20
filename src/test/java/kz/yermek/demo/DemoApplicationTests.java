5c856d5dfe1a6698b02f22bf598fe1e9d3bba0d2
Ermek Shmanov <erma_99-22@mail.ru>
2023-08-20 21:43:12 +0300

    пустой проект gradle для добавления других модулей

---
+++ src/test/java/kz/yermek/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.yermek.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
