cbc4e4e963a777e4cf0980f58c64586e0a95c48c
Ermek Shmanov <erma_99-22@mail.ru>
2023-08-21 19:04:26 +0300

    доработанный проект для клиента eureka с добавленным RESTful сервисом

---
+++ eureka-client/src/main/java/kz/yermek/micro/demo/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,14 @@
+package kz.yermek.micro.demo.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+public class EurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientApplication.class, args);
+	}
+
+}
