1677ec71b8812bfe1b29efe31b7279e5a52f9e1c
Ermek Shmanov <erma_99-22@mail.ru>
2023-08-22 20:40:10 +0300

    first commit

---
+++ api-gateway/src/test/java/kz/yermek/micro/demo/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.yermek.micro.demo.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ eureka-client-2/src/test/java/kz/yermek/micro/demo/eurekaclient2/EurekaClient2ApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.yermek.micro.demo.eurekaclient2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClient2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
